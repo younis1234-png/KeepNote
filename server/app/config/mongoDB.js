a1084c12ca812745f0d47d124190b9b369433904
Younis <younis@Youniss-MacBook-Pro.local>
2021-05-01 08:32:32 -0400

    Worked on the back end today connect mongodb and create routes

---
+++ server/app/config/mongoDB.js
@@ -0,0 +1,12 @@
+let MONGO_DB = require("mongodb");
+const MongoClient = MONGO_DB.MongoClient;
+const url = process.env.MONGO_URL;
+
+try {
+  let M_CONNECT = MongoClient.connect(url, {
+    useUnifiedTopology: true,
+  });
+  module.exports = { MONGO_DB, M_CONNECT };
+} catch (err) {
+  console.log(err);
+}
